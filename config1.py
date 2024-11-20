ffa21a96b103c2dfdc5cc895cff6e6b7b6bde0f4
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-11-20 19:04:48 +0530

    Update config1.py

--- config1.py
+++ config1.py
@@ -6,9 +6,10 @@ API_PASSPHRASE = "9025014840"
 # 3Commas webhook details
 THREE_COMMAS_WEBHOOK_URL = "https://api.3commas.io/signal_bots/webhooks"
 SECRET_1 = "eyJhbGciOiJIUzI1NiJ9.eyJzaWduYWxzX3NvdXJjZV9pZCI6MTAyNTR9.UpdSFm3FpcmR7hcVaBUOzPp5Q6-1gBRlQA21sH0ieHo"
-
+SECRET_2 = "eyJhbGciOiJIUzI1NiJ9.eyJzaWduYWxzX3NvdXJjZV9pZCI6ODI4MzV9.ccZIwRRxe781qP0Mtlx62NIkmKyUGs155h433uxE-M0"
 # Telegram Bot Token
 TELEGRAM_TOKEN = "7619077339:AAGvLzsABJRFKsv50TgI1XxMNhVvtED-E_4"
 
 # Trading Bot UUID
-BOT_UUID = "d91199bc-3c4e-4938-8bbb-f5ed772fe89f"
+BOT_UUID_1 = "d91199bc-3c4e-4938-8bbb-f5ed772fe89f"
+BOT_UUID_2 = "94de674e-e3f6-4c9f-9cc2-4a70e9acf79d"
