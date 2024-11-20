465639e4ad97ff6a379680e9b3f35320b03b1f4b
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-11-20 17:30:37 +0530

    Update config1.py

--- config1.py
+++ config1.py
@@ -1,14 +1,14 @@
 # Bitget API credentials
-API_KEY = "your_bitget_api_key"
-API_SECRET = "your_bitget_api_secret"
-API_PASSPHRASE = "your_bitget_api_passphrase"
+API_KEY = 'bg_0b01ffa94b4b6f3c2ba5df488587a91e'
+API_SECRET = 'facbe7d57bbfb8c6fd82a39d2ce5dc35eef578058ec667aa5ad2fc02dffa644a'
+API_PASSPHRASE = '9025014840'
 
 # 3Commas webhook details
-THREE_COMMAS_WEBHOOK_URL = "your_3commas_webhook_url"
-SECRET_1 = "your_3commas_secret"
+THREE_COMMAS_WEBHOOK_URL = 'https://api.3commas.io/signal_bots/webhooks'
+SECRET_1 = 'eyJhbGciOiJIUzI1NiJ9.eyJzaWduYWxzX3NvdXJjZV9pZCI6MTAyNTR9.UpdSFm3FpcmR7hcVaBUOzPp5Q6-1gBRlQA21sH0ieHo'
 
 # Telegram Bot Token
-TELEGRAM_TOKEN = "your_telegram_bot_token"
+TELEGRAM_TOKEN = '7619077339:AAGvLzsABJRFKsv50TgI1XxMNhVvtED-E_4'
 
 # Trading Bot UUID
-BOT_UUID = "your_trading_bot_uuid"
+BOT_UUID = 'd91199bc-3c4e-4938-8bbb-f5ed772fe89f'
